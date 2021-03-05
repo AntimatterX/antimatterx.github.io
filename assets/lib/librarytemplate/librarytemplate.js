f65182fca7485bb04c79d1a77f16ba0bc454b554
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-03-06 01:06:00 +0900

    commit

--- assets/lib/librarytemplate/librarytemplate.js
+++ assets/lib/librarytemplate/librarytemplate.js
@@ -1,5 +1,5 @@
 /*!
- * LibraryTemplate JavaScript Library v2.1.1
+ * LibraryTemplate JavaScript Library v2.2.0
  * https://github.com/AntimatterX/antimatterx.github.io/blob/main/assets/lib/librarytemplate/librarytemplate.js
  * 
  * Copyright (c) 2021 AntimatterX
@@ -10,7 +10,7 @@
  * This library is declared globally in non-Node.js environments with the following name.
  * librarytemplate
  * 
- * Last Update: 2021-02-27T08:27:43.180Z
+ * Last Update: 2021-03-05T16:03:12.310Z
  */
 (function _main(_root, undefined) {
     'use strict';
@@ -31,7 +31,7 @@
             },
             /**
              * 型名を返します。
-             * @param {*?} [x=undefined] 型名を取得する値を渡します。
+             * @param {?*} [x=undefined] 型名を取得する値を渡します。
              * @returns {string} 第一引数に渡した値の型名の文字列を返します。
              */
             getType: function (x) {
@@ -39,7 +39,7 @@
             },
             /**
              * 指定した型名か判定します。
-             * @param {*?} x 判定する値を渡します。
+             * @param {?*} x 判定する値を渡します。
              * @param {string|Array<string>} type 型名の文字列または型名の配列を渡します。
              * @returns {boolean} 第一引数に渡した値の型名が第二引数に渡された型名か判定して真偽値で返します。
              */
@@ -49,21 +49,45 @@
                     case 'String':
                         return xType === type;
                     case 'Array':
-                        return type.indexOf(xType) > 0;
+                        return type.indexOf(xType) > -1;
                     default:
                         return false;
                 }
             },
             /**
              * デフォルト値の型にキャストします。
-             * @param {*?} x キャストする値を渡します。
-             * @param {*?} defaultValue デフォルトの値を渡します。
+             * @param {?*} x キャストする値を渡します。
+             * @param {?*} defaultValue デフォルトの値を渡します。
              * @param {string|Array<string>} [allowType=[]] デフォルト値の型以外にも許容する型の型名の文字列または型名の配列を渡します。
              * @returns {?*} デフォルト値の型にキャストされた値を返します。
              */
-            caseType: function (x, defaultValue, allowType) {
+            castType: function (x, defaultValue, allowType) {
                 return _ctx.fn.isType(x, [_ctx.fn.getType(defaultValue)].concat(Array.isArray(allowType) ? allowType :
                     typeof allowType === 'string' ? [allowType] : [])) ? x : defaultValue;
+            },
+            /**
+             * オブジェクトのプロパティをデフォルト値のプロパティの型にキャストします。
+             * @param {Object<string, ?*>|Array<?*>} param キャストするオブジェクトを渡します。
+             * @param {Object<string, ?*>|Array<?*>} defaultParam デフォルト値のオブジェクトを渡します。
+             * @param {Array<string|Array<string>>} [allowTypes=[]] デフォルト値のオブジェクトと同じ長さのデフォルト値のプロパティの型以外にも許容する型の型名の文字列または型名の配列を含む配列を渡します。
+             * @returns {Object<string, ?*>} デフォルト値の型にキャストされたオブジェクトを返します。
+             */
+            castParam: function (param, defaultParam, allowTypes) {
+                param = _ctx.fn.castType(param, {});
+                defaultParam = _ctx.fn.castType(defaultParam, {});
+                allowTypes = _ctx.fn.castType(allowTypes, []).slice();
+                var clone = (function () {
+                    var obj = {};
+                    Object.keys(param).forEach(function (k) {
+                        obj[k] = param[k];
+                    });
+                    return obj;
+                })();
+                Object.keys(defaultParam).forEach(function (k, i) {
+                    if (!(k in clone)) clone[k] = defaultParam[k];
+                    else clone[k] = _ctx.fn.castType(clone[k], defaultParam[k], _ctx.fn.castType(allowTypes[i], [], 'String'));
+                });
+                return clone;
             }
         }
     };
